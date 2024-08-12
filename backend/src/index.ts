6027c1be42b50cd8d4a40f280451750b86f21857
tushar_singh <tscorpo2@gmail.com>
2024-08-12 21:00:15 +0530

    addedsignupsigninroute

---
+++ backend/src/index.ts
@@ -0,0 +1,72 @@
+import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, sign, verify } from 'hono/jwt'
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string,
+    JWT_SECRET: string
+	}
+}>();
+app.use('/api/v1/blog/*', async (c, next) => {
+  //middleware
+  //checkign the header 
+  //if failed return a 403 to the user
+  const header = c.req.header("authorization") || "";
+  const token = header.split(" ")[1];
+  const response = await verify(token,c.env.JWT_SECRET);
+  if(response.id){
+    await next()
+  }
+  else{
+    c.status(403);
+    return c.json({error: "unauthorized"});
+  }
+ 
+})
+app.post('/api/v1/user/signup',async (c)=>{
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  const body = await c.req.json();
+  const user = await prisma.user.create({
+    data : {
+      email: body.email,
+      password : body.password,
+    }
+  })
+  const token = await sign({id : user.id},c.env.JWT_SECRET);
+  return c.json({jwt : token});
+})
+app.post('/api/v1/user/signin',async (c)=>{
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  const body = await c.req.json();
+  const user = await prisma.user.findUnique({
+    where : {
+      email : body.email,
+      password : body.password,
+    }
+  });
+  if(!user){
+    c.status(403);
+    return c.json({error : "user not found"});
+  }
+  const token = await sign({id : user.id},"secret");
+  return c.json({jwt : token});
+})
+app.post('/api/v1/blog',(c)=>{
+  return c.text("");
+})
+app.put('/api/v1/blog',(c)=>{
+  return c.text("");
+})
+app.get('/api/v1/blog/:id',(c)=>{
+  return c.text("");
+})
+app.get('/api/v1/blog/bulk',(c)=>{
+  return c.text("");
+})
+
+export default app
